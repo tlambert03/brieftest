a637d49afc5ec74c428ed21b61e8389cab23db7c
Talley Lambert <talley.lambert@gmail.com>
2020-05-25 11:49:03 -0400

    just support

---
+++ download.py
@@ -0,0 +1,23 @@
+import requests
+
+url = 'https://briefcase-support.org/python?platform=linux&version=3.8&arch=x86_64'
+filename = 'support.tar.gz'
+
+response = requests.get(url, stream=True)
+with filename.open("wb") as f:
+    total = response.headers.get("content-length")
+    if total is None:
+        f.write(response.content)
+    else:
+        downloaded = 0
+        total = int(total)
+        for data in response.iter_content(chunk_size=1024 * 1024):
+            downloaded += len(data)
+            f.write(data)
+            done = int(50 * downloaded / total)
+            print(
+                "\r{}{} {}%".format("#" * done, "." * (50 - done), 2 * done),
+                end="",
+                flush=True,
+            )
+print(filename)
