e66448bb651f8a31118a0a8f0c99b107713999d3
Talley Lambert <talley.lambert@gmail.com>
2020-05-27 12:41:55 -0400

    back to linux with simple app

--- brieftest/app.py
+++ brieftest/app.py
@@ -2,22 +2,24 @@
 My first application
 """
 import sys
-from PySide2 import QtWidgets, QtCore
-import numpy as np
+
+# from PySide2 import QtWidgets, QtCore
 
 
-class HelloWorld(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.init_ui()
+# class HelloWorld(QtWidgets.QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         self.init_ui()
 
-    def init_ui(self):
-        self.setWindowTitle("brieftest")
-        self.show()
+#     def init_ui(self):
+#         self.setWindowTitle("brieftest")
+#         self.show()
 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
-    _ = HelloWorld()
-    QtCore.QTimer().singleShot(1000, app.quit)
-    sys.exit(app.exec_())
+    import numpy as np
+
+    # app = QtWidgets.QApplication(sys.argv)
+    # _ = HelloWorld()
+    # QtCore.QTimer().singleShot(1000, app.quit)
+    # sys.exit(app.exec_())
